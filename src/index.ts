76b48203a183c9507d67a1e0d138add19bd1500d
Petr Plavjanik <petr.plavjanik@broadcom.com>
2020-08-08 11:53:57 +0200

    wip!

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,11 @@
 import {Command, flags} from '@oclif/command'
 import {existsSync, readFileSync, writeFileSync} from 'fs'
-import {resolve} from 'path'
-import {checkZowe} from './zowe'
+import {join, resolve} from 'path'
 import * as tmp from 'tmp'
+import {AbstractSession, CliProfileManager, Imperative, ImperativeConfig, IProfileLoaded} from '@zowe/imperative'
+import {Create, CreateDataSetTypeEnum, List, ZosmfSession, Upload, sleep} from "@zowe/cli";
+import {PerfTiming} from "@zowe/perf-timing";
+
 const filesizeParser = require('filesize-parser')
 
 interface TestDefinition {
@@ -18,6 +21,22 @@ interface TestDefinition {
   dsnSecondSegment: string;
 }
 
+async function initializeImperative() {
+  const mainZoweDir = join(require.resolve('@zowe/cli'), '..', '..', '..', '..');
+  (process.mainModule as any).filename = require.resolve('@zowe/cli');
+  ((process.mainModule as any).paths as any).unshift(mainZoweDir);
+  await Imperative.init({configurationModule: require.resolve('@zowe/cli/lib/imperative.js')})
+}
+
+async function datasetExists(session: AbstractSession, dsn: string): Promise<boolean> {
+  const response = await List.dataSet(session, dsn)
+  try {
+    return response.apiResponse.returnedRows === 1
+  } catch (error) {
+    return false
+  }
+}
+
 class Zztop extends Command {
   static description = 'describe the command here'
 
@@ -31,6 +50,9 @@ class Zztop extends Command {
 
   async run() {
     const {args} = this.parse(Zztop)
+    if (!PerfTiming.isEnabled) {
+      this.error("PerfTiming is not enabled. Set environment variables: PERF_TIMING_ENABLED=TRUE PERF_TIMING_IO_MAX_HISTORY=1 PERF_TIMING_IO_SAVE_DIR=.", {exit: 1})
+    }
 
     this.log(`Test definition file: ${args.file}`)
     if (!existsSync(args.file)) {
@@ -38,15 +60,57 @@ class Zztop extends Command {
     }
     const testDefinition: TestDefinition = JSON.parse(readFileSync(args.file, 'utf8'))
     this.log(`${JSON.stringify(testDefinition, null, 2)}`)
+    await initializeImperative();
 
-    checkZowe(this, testDefinition.zosmfProfiles)
+    const profiles = await new CliProfileManager({
+      profileRootDirectory: join(ImperativeConfig.instance.cliHome, 'profiles'),
+      type: `zosmf`
+    }).loadAll();
 
-    const tmpCobolPath = tmp.tmpNameSync()
-    const lineCount = filesizeParser(testDefinition.memberSize) / 80
-    const result = " 04110     DISPLAY 'HELLO, WORLD' UPON CONSL.                           00170000".repeat(lineCount)
-    writeFileSync(tmpCobolPath, result)
+    const zosmfProfiles = profiles.filter((profile) => {
+      return profile.type === `zosmf`;
+    });
 
-    // const userid = testDefinition.
+    let zosmfProfilesByName: { [name: string]: IProfileLoaded } = {}
+
+    for (const profile of zosmfProfiles) {
+      if (profile.name) {
+        zosmfProfilesByName[profile.name] = profile
+      }
+    }
+
+    const profileName = testDefinition.zosmfProfiles[0]
+    const profile = zosmfProfilesByName[profileName]
+    if (profile && profile.profile) {
+      const userid = profile.profile.user
+      this.log(`Userid: ${userid}`)
+      const session = ZosmfSession.createBasicZosmfSession(profile.profile);
+
+      const tmpCobolPath = tmp.tmpNameSync()
+      const lineCount = filesizeParser(testDefinition.memberSize) / 80
+      const result = " 04110     DISPLAY 'HELLO, WORLD' UPON CONSL.                           00170000\n".repeat(lineCount)
+      writeFileSync(tmpCobolPath, result)
+      const testDsn = userid.toUpperCase() + "." + testDefinition.dsnSecondSegment + ".TEST1"
+      const exists = await datasetExists(session, testDsn)
+      if (!exists) {
+        const response = await Create.dataSet(session, CreateDataSetTypeEnum.DATA_SET_CLASSIC, testDsn)
+        if (!response.success) {
+          this.error(response.commandResponse, {exit: 1})
+        }
+      }
+      PerfTiming.api.mark("BeforeDatasetUpload")
+      const uploadResponse = await Upload.fileToDataset(session, tmpCobolPath, testDsn + "(MEMBER1)")
+      PerfTiming.api.mark("AfterDatasetUpload")
+      this.log(JSON.stringify(uploadResponse))
+    } else {
+      this.error(`Invalid profile name: ${profileName}`)
+    }
+
+    PerfTiming.api.measure("DatasetUpload", "BeforeDatasetUpload", "AfterDatasetUpload")
+    await sleep(1000)
+    this.log(JSON.stringify(PerfTiming.api.getMetrics().measurements, null, 2))
+
+
     // const dsn =
     // if (!zosExistsSync()
     // zowe  zos-files create data-set-classic NEW.CLASSIC.DATASET
