1d87562618286e730bb4194c64ea9b12e2c2192a
Petr Plavjanik <petr.plavjanik@broadcom.com>
2020-09-10 09:43:43 +0200

    More tests (#3)
    * Log requests to a log file
    
    * Jobs and other CLI commands
    
    * Test data cleanup and eslint
    
    * Try catch for TS command
    
    Co-authored-by: JirkaAichler <jiri.aichler@broadcom.com>

--- README.md
+++ README.md
@@ -11,11 +11,17 @@ Zowe z/OSMF Test of Performance
 
 ## Description
 
-<https://docs.google.com/document/d/1UEOSERYf7qSXGZY-w1aqI8kBfjweIRTk8zRKlBGbh_4/edit?usp=sharing>
+The purpose of this CLI tool is to generate workload for z/OSMF that is typical for Zowe CLI use cases.
+It is configurable, you can specify number of users, duration, and size of the data used during the tests.
+
+The resource consumption on z/OS is not measured by this tool, you need to measure it on z/OS 
+- all z/OSMF (IZU*) address space, CIM server, and all TSO sessions created for the test users.
+
+Design document: <https://docs.google.com/document/d/1UEOSERYf7qSXGZY-w1aqI8kBfjweIRTk8zRKlBGbh_4/edit?usp=sharing>
 
 ## Requirements
 
-- Node.js 12 and above on any platform 
+- Node.js 12 and above on any platform (Linux, Windows, [z/OS](https://docs.zowe.org/stable/user-guide/install-nodejs-zos.html))
 
 ## Usage
 
@@ -35,9 +41,9 @@ Zowe z/OSMF Test of Performance
    npx zowe profiles create zosmf-profile zzow01-zowep --host zzow01.zowe.marist.cloud --port 10443 --user userid --pass "passwd" --reject-unauthorized false --overwrite
    ```
    
-   Set the host and port to the values of the tested z/OSMF instance.
+   Set the host and port to the values of the tested z/OSMF instance. Use a different profile name instead of `zzow01-zowep` for each user.
 
-3. Create test definition file `test.json`:
+3. Create test definition file `test.json` - example:
 
     ```json
     {
@@ -45,22 +51,45 @@ Zowe z/OSMF Test of Performance
       "fileSize": "10k",
       "memberSize": "10k",
       "jobOutputSize": "10k",
-      "tsoCommandOutputSize": "1k",
-      "duration": "15s",
+      "duration": "5m",
       "commandDelay": "1s",
-      "scriptDelay": "1s",
-      "concurrentUsers": 5,
+      "scriptDelay": "15s",
+      "concurrentUsers": 10,
       "zosmfProfiles": ["zzow01-zowep"],
       "dsnSecondSegment": "ZZTOP",
-      "unixDir": "/zaas1/zowep/zztop"
+      "unixDir": "/zaas1/zowep/zztop",
+      "accountCode": "00000000",
+      "jobCard": [
+        "//$jobname JOB 000000000,'ZZTOP',MSGCLASS=A,CLASS=A,",
+        "//  MSGLEVEL=(1,1),REGION=0M",
+        "/*JOBPARM SYSAFF=*"
+      ]   
     }
     ```
    
    Use same profile names as in the step #2. Their number can be lower than the number of concurrent users.
-   Provide valid values for `dsnSecondSegment` and `unixDir`.
+   Provide valid values for `jobCard`, `dsnSecondSegment` and `unixDir`.
+   
+   You can start with a short `duration` and lower number of `concurrentUsers` and then you can increase it e.g. 1 hour 
+   and try to maximum amount of users.  
+   
+   Available time unit types are:
+    
+   - nanoseconds (ns)
+   - microseconds (μs)
+   - milliseconds (ms)
+   - seconds (s, sec)
+   - minutes (m, min)
+   - hours (h, hr)
+   - days (d)
+   - weeks (w, wk)
+   - months
+   - years (y, yr)   
 
 4. Run it:
 
     ```bash
     PERF_TIMING_ENABLED=TRUE PERF_TIMING_IO_MAX_HISTORY=1 PERF_TIMING_IO_SAVE_DIR=. npx @zowedev/zztop test.json
     ```
+   
+5. Capture `requests.log`, `requests-error.log`, `metrics.1.json`, and the output of the command.   
--- src/index.ts
+++ src/index.ts
@@ -10,24 +10,51 @@ import {
   IProfileLoaded,
   Session,
 } from '@zowe/imperative'
-import {Create, CreateDataSetTypeEnum, Download, List, sleep, Upload, ZosmfSession} from '@zowe/cli'
+import {
+  Create,
+  CreateDataSetTypeEnum, Delete,
+  DeleteJobs,
+  Download,
+  GetJobs,
+  IssueCommand,
+  IssueTso,
+  List,
+  MonitorJobs,
+  sleep,
+  SubmitJobs,
+  Upload,
+  ZosmfSession,
+} from '@zowe/cli'
 import {PerfTiming} from '@zowe/perf-timing'
 import parse from 'parse-duration'
 
 const filesizeParser = require('filesize-parser')
+const winston = require('winston')
+
+const logger = winston.createLogger({
+  level: 'info',
+  format: winston.format.json(),
+  defaultMeta: {service: 'user-service'},
+  transports: [
+    new winston.transports.File({filename: 'requests-error.log', level: 'error'}),
+    new winston.transports.File({filename: 'requests.log'}),
+  ],
+})
 
 interface TestDefinition {
   name: string;
   fileSize: string;
   memberSize: string;
   jobOutputSize: string;
-  tsoCommandOutputSize: string;
   duration: string;
   commandDelay: string;
   scriptDelay: string;
   concurrentUsers: number;
   zosmfProfiles: string[];
   dsnSecondSegment: string;
+  unixDir: string;
+  accountCode: string;
+  jobCard: string[];
 }
 
 interface ActivityStats {
@@ -52,7 +79,7 @@ async function datasetExists(session: AbstractSession, dsn: string): Promise<boo
 }
 
 class Zztop extends Command {
-  static description = 'describe the command here'
+  static description = 'Zowe z/OSMF Test of Performance'
 
   static flags = {
     // add --version flag to show CLI version
@@ -75,69 +102,136 @@ class Zztop extends Command {
       return {failedRequests: 0, successfulRequests: 0}
     }
 
-    const driver = this // eslint-disable-line no-this-alias
     const userid = profile.profile.user
     this.log(`Userid #${userNumber}: ${userid}`)
     const session = ZosmfSession.createBasicZosmfSession(profile.profile)
 
-    const {tmpCobolPath, testDsn} = await this.prepareTestData(testDefinition, userid, userNumber, session)
+    const {tmpCobolPath, testDsn, testJobname, testJobid, testSpoolId, testJcl, testUploadUssPath} = await this.prepareTestData(testDefinition, userid, userNumber, session)
+
+    this.log(`Running tests for ${userNumber} - ${userid}`)
 
     const tests = [
       // zowe files upload ftds
       {
         name: 'DatasetUpload', action: async function () {
-          const uploadResponse = await Upload.fileToDataset(session, tmpCobolPath, testDsn + '(TEST1)')
-          driver.log(JSON.stringify(uploadResponse))
-          return uploadResponse.success
+          return Upload.fileToDataset(session, tmpCobolPath, testDsn + '(TEST1)')
         },
       },
       // zowe files download ds
       {
         name: 'DatasetDownload', action: async function () {
           const tmpDownloadPath = tmp.tmpNameSync()
-          const uploadResponse = await Download.dataSet(session, testDsn + '(TEST1)', {file: tmpDownloadPath})
-          driver.log(JSON.stringify(uploadResponse))
+          const response = await Download.dataSet(session, testDsn + '(TEST1)', {file: tmpDownloadPath})
           unlinkSync(tmpDownloadPath)
-          return uploadResponse.success
+          return response
         },
       },
-      // TODO:
       // zowe files upload ftu
+      {
+        name: 'FileUpload', action: async function () {
+          return Upload.fileToUssFile(session, tmpCobolPath, testUploadUssPath)
+        },
+      },
       // zowe files download uf
+      {
+        name: 'FileDownload', action: async function () {
+          const tmpDownloadPath = tmp.tmpNameSync()
+          const response = await Download.ussFile(session, `${testDefinition.unixDir}/test${userNumber}.txt`, {file: tmpDownloadPath})
+          unlinkSync(tmpDownloadPath)
+          return response
+        },
+      },
       // zowe tso issue command
+      {
+        name: 'TsoCommand', action: async function () {
+          try {
+            const response = await IssueTso.issueTsoCommand(session, testDefinition.accountCode, `SEND 'Hello' USER(${userid})`)
+            return response
+          }
+          catch (error) {
+            return { success: false, error: error }
+          }
+        },
+      },
       // zowe console issue command
-      // zowe console collect sr
-      // zowe jobs submit
-      // zowe jobs view
-      // zowe jobs download
+      {
+        name: 'ConsoleCommand', action: async function () {
+          return IssueCommand.issue(session, {command: 'D IPLINFO'})
+        },
+      },
+      // zowe jobs submit [PP]
+      {
+        name: 'JobSubmit', action: async function () {
+          const job = await SubmitJobs.submitJcl(session, testJcl)
+          const cleanup = async function () {
+            await MonitorJobs.waitForOutputStatus(session, job.jobname, job.jobid)
+            await DeleteJobs.deleteJob(session, job.jobname, job.jobid)
+          }
+          const promise = cleanup()
+          return {success: true, job: job, cleanupPromise: promise}
+        },
+      },
+      // zowe jobs view [PP]
+      {
+        name: 'JobView', action: async function () {
+          const spoolFiles = await GetJobs.getSpoolFiles(session, testJobname, testJobid)
+          return {success: spoolFiles.length > 0, spoolFiles: spoolFiles}
+        },
+      },
+      // zowe jobs download [PP]
+      {
+        name: 'JobDownload', action: async function () {
+          const spoolContent = await GetJobs.getSpoolContentById(session, testJobname, testJobid, testSpoolId)
+          return {success: spoolContent.length > 0, content: spoolContent}
+        },
+      },
     ]
 
     let successfulRequests = 0
     let failedRequests = 0
     const startTime = new Date().getTime()
+    let requestNumber = 0
     while (new Date().getTime() - startTime <= duration) {
       const scriptStartTime = new Date().getTime()
+      PerfTiming.api.mark('BeforeScript' + userNumber)
 
       for (const test of tests) {
+        requestNumber++
         const commandStartTime = new Date().getTime()
-        PerfTiming.api.mark('Before' + test.name)
-        const success = await test.action() // eslint-disable-line no-await-in-loop
-        PerfTiming.api.mark('After' + test.name)
-        if (success) {
-          PerfTiming.api.measure(test.name, 'Before' + test.name, 'After' + test.name)
+        PerfTiming.api.mark('Before' + test.name + userNumber)
+        const response = await test.action() // eslint-disable-line no-await-in-loop
+        PerfTiming.api.mark('After' + test.name  + userNumber)
+        const responseString = JSON.stringify(response)
+        if (response.success) {
+          PerfTiming.api.measure(test.name, 'Before' + test.name + userNumber, 'After' + test.name + userNumber)
           successfulRequests++
+          logger.info(`User #${userNumber} ${userid} - Request ${requestNumber} - Test ${test.name}: ${responseString}`)
         } else {
-          PerfTiming.api.measure(test.name + 'Failed', 'Before' + test.name, 'After' + test.name)
+          PerfTiming.api.measure(test.name + 'Failed', 'Before' + test.name + userNumber, 'After' + test.name + userNumber)
           failedRequests++
+          logger.error(`User #${userNumber} ${userid} - Request ${requestNumber} - Test ${test.name}: ${responseString}`)
         }
 
         await sleep(Math.max(commandDelay - (new Date().getTime() - commandStartTime), 0)) // eslint-disable-line no-await-in-loop
       }
+      PerfTiming.api.mark('AfterScript' + userNumber)
+      PerfTiming.api.measure('Script', 'BeforeScript' + userNumber, 'AfterScript' + userNumber)
 
       await sleep(Math.max(scriptDelay - (new Date().getTime() - scriptStartTime), 0)) // eslint-disable-line no-await-in-loop
     }
 
+    this.log(`Finished tests for ${userNumber} - ${userid}`)
+    await this.cleanupTestData(session, userNumber, testJobname, testJobid, testDsn, testUploadUssPath, tmpCobolPath)
+
     return {failedRequests: failedRequests, successfulRequests: successfulRequests}
+  }
+
+  private async cleanupTestData(session: Session, userNumber: number, testJobname: string, testJobid: string, testDsn: string, testUploadUssPath: string, tmpCobolPath: string) {
+    this.log(`Cleaning up test data for user #${userNumber}`)
+    await DeleteJobs.deleteJob(session, testJobname, testJobid)
+    await Delete.dataSet(session, testDsn)
+    await Delete.ussFile(session, testUploadUssPath)
+    unlinkSync(tmpCobolPath)
   }
 
   async run() {
@@ -186,7 +280,7 @@ class Zztop extends Command {
     }
 
     await sleep(1000)
-    const testNames = ['DatasetUpload', 'DatasetDownload']
+    const testNames = ['DatasetUpload', 'DatasetDownload', 'FileUpload', 'FileDownload', 'TsoCommand', 'ConsoleCommand', 'JobSubmit', 'JobView', 'JobDownload', 'Script']
     for (const measurement of PerfTiming.api.getMetrics().measurements) {
       for (const testName of testNames) {
         if (measurement.name === testName) {
@@ -201,10 +295,23 @@ class Zztop extends Command {
   }
 
   private async prepareTestData(testDefinition: TestDefinition, userid: string, userNumber: number, session: Session) {
+    const tmpCobolPath = await this.prepareTestFile(testDefinition)
+    const testDsn = await this.prepareTestDataset(userid, testDefinition, userNumber, session)
+    const testUploadUssPath = `${testDefinition.unixDir}/test${userNumber}.txt`
+    const {testJcl, testJobid, testJobname, testSpoolId} = await this.prepareTestJob(testDefinition, userNumber, session)
+    this.log('Prepared test data', userNumber, '-', tmpCobolPath, testDsn, testJobname, testJobid, testSpoolId, testUploadUssPath)
+    return {tmpCobolPath, testDsn, testJobname, testJobid, testSpoolId, testJcl, testUploadUssPath}
+  }
+
+  private async prepareTestFile(testDefinition: TestDefinition) {
     const tmpCobolPath = tmp.tmpNameSync()
     const lineCount = filesizeParser(testDefinition.memberSize) / 80
     const result = " 04110     DISPLAY 'HELLO, WORLD' UPON CONSL.                           00170000\n".repeat(lineCount)
     await promises.writeFile(tmpCobolPath, result)
+    return tmpCobolPath
+  }
+
+  private async prepareTestDataset(userid: string, testDefinition: TestDefinition, userNumber: number, session: Session) {
     const testDsn = userid.toUpperCase() + '.' + testDefinition.dsnSecondSegment + `.U${userNumber}`
     this.log(testDsn)
     const exists = await datasetExists(session, testDsn)
@@ -214,7 +321,35 @@ class Zztop extends Command {
         this.error(response.commandResponse, {exit: 2})
       }
     }
-    return {tmpCobolPath, testDsn}
+    return testDsn
+  }
+
+  private async prepareTestJob(testDefinition: TestDefinition, userNumber: number, session: Session) {
+    const jobCard = testDefinition.jobCard.join('\n').replace('$jobname', `ZZT${userNumber}`)
+    const jobLines = [
+      '//RUN EXEC PGM=IEBGENER',
+      '//SYSPRINT DD SYSOUT=*',
+      '//SYSIN DD DUMMY',
+      '//SYSUT2 DD SYSOUT=*',
+      '//SYSUT1 DD *',
+    ]
+    const sysut1LineCount = filesizeParser(testDefinition.jobOutputSize) / 80
+    const testJcl = jobCard + '\n' + jobLines.join('\n') + '\n' + " 04110     DISPLAY 'HELLO, WORLD' UPON CONSL.                           00170000\n".repeat(sysut1LineCount)
+    const job = await SubmitJobs.submitJclNotifyCommon(session, {jcl: testJcl})
+    if (job.retcode !== 'CC 0000') {
+      this.error(JSON.stringify(job), {exit: 2})
+    }
+    const testJobid = job.jobid
+    const testJobname = job.jobname
+
+    const jobFiles = await GetJobs.getSpoolFiles(session, testJobname, testJobid)
+    let testSpoolId = 0
+    for (const jobFile of jobFiles) {
+      if (jobFile.ddname === 'SYSUT2') {
+        testSpoolId = jobFile.id
+      }
+    }
+    return {testJcl, testJobid, testJobname, testSpoolId}
   }
 }
 
