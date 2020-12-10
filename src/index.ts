d606b991436de493057968d6b38901e2f3351ec4
Petr Plavjanik <petr.plavjanik@broadcom.com>
2020-12-10 12:46:35 +0100

    Handle errors in data preparation
    Signed-off-by: Petr Plavjanik <petr.plavjanik@broadcom.com>

--- src/index.ts
+++ src/index.ts
@@ -157,216 +157,237 @@ class Zztop extends Command {
     this.log(`Userid #${userNumber}: ${userid}`);
     const session = ZosmfSession.createBasicZosmfSession(profile.profile);
 
-    const {
-      tmpCobolPath,
-      testDsn,
-      testJobname,
-      testJobid,
-      testSpoolId,
-      testJcl,
-      testUploadUssPath,
-    } = await this.prepareTestData(testDefinition, userid, userNumber, session);
+    try {
+      const {
+        tmpCobolPath,
+        testDsn,
+        testJobname,
+        testJobid,
+        testSpoolId,
+        testJcl,
+        testUploadUssPath,
+      } = await this.prepareTestData(
+        testDefinition,
+        userid,
+        userNumber,
+        session
+      );
 
-    this.log(`Running tests for ${userNumber} - ${userid}`);
+      this.log(`Running tests for ${userNumber} - ${userid}`);
 
-    const allTests: Test[] = [
-      // zowe files upload ftds
-      {
-        name: "DatasetUpload",
-        action: async function () {
-          return Upload.fileToDataset(
-            session,
-            tmpCobolPath,
-            testDsn + "(TEST1)"
-          );
-        },
-      },
-      // zowe files download ds
-      {
-        name: "DatasetDownload",
-        action: async function () {
-          const tmpDownloadPath = tmp.tmpNameSync();
-          const response = await Download.dataSet(
-            session,
-            testDsn + "(TEST1)",
-            { file: tmpDownloadPath }
-          );
-          unlinkSync(tmpDownloadPath);
-          return response;
-        },
-      },
-      // zowe files upload ftu
-      {
-        name: "FileUpload",
-        action: async function () {
-          return Upload.fileToUssFile(session, tmpCobolPath, testUploadUssPath);
-        },
-      },
-      // zowe files download uf
-      {
-        name: "FileDownload",
-        action: async function () {
-          const tmpDownloadPath = tmp.tmpNameSync();
-          const response = await Download.ussFile(
-            session,
-            `${testDefinition.unixDir}/test${userNumber}.txt`,
-            { file: tmpDownloadPath }
-          );
-          unlinkSync(tmpDownloadPath);
-          return response;
-        },
-      },
-      // zowe tso issue command
-      {
-        name: "TsoCommand",
-        action: async function () {
-          const response = await IssueTso.issueTsoCommand(
-            session,
-            testDefinition.accountCode,
-            `SEND 'Hello' USER(${userid})`
-          );
-          return response;
-        },
-      },
-      // zowe console issue command
-      {
-        name: "ConsoleCommand",
-        action: async function () {
-          return IssueCommand.issue(session, { command: "D IPLINFO" });
-        },
-      },
-      // zowe jobs submit
-      {
-        name: "JobSubmit",
-        action: async function () {
-          const job = await SubmitJobs.submitJcl(session, testJcl);
-          const cleanup = async function () {
-            await MonitorJobs.waitForOutputStatus(
+      const allTests: Test[] = [
+        // zowe files upload ftds
+        {
+          name: "DatasetUpload",
+          action: async function () {
+            return Upload.fileToDataset(
               session,
-              job.jobname,
-              job.jobid
+              tmpCobolPath,
+              testDsn + "(TEST1)"
             );
-            await DeleteJobs.deleteJob(session, job.jobname, job.jobid);
-          };
-          const promise = cleanup();
-          return { success: true, job: job, cleanupPromise: promise };
+          },
         },
-      },
-      // zowe jobs view
-      {
-        name: "JobView",
-        action: async function () {
-          const spoolFiles = await GetJobs.getSpoolFiles(
-            session,
-            testJobname,
-            testJobid
-          );
-          return { success: spoolFiles.length > 0, spoolFiles: spoolFiles };
+        // zowe files download ds
+        {
+          name: "DatasetDownload",
+          action: async function () {
+            const tmpDownloadPath = tmp.tmpNameSync();
+            const response = await Download.dataSet(
+              session,
+              testDsn + "(TEST1)",
+              { file: tmpDownloadPath }
+            );
+            unlinkSync(tmpDownloadPath);
+            return response;
+          },
         },
-      },
-      // zowe jobs download
-      {
-        name: "JobDownload",
-        action: async function () {
-          const spoolContent = await GetJobs.getSpoolContentById(
-            session,
-            testJobname,
-            testJobid,
-            testSpoolId
-          );
-          return { success: spoolContent.length > 0, content: spoolContent };
+        // zowe files upload ftu
+        {
+          name: "FileUpload",
+          action: async function () {
+            return Upload.fileToUssFile(
+              session,
+              tmpCobolPath,
+              testUploadUssPath
+            );
+          },
         },
-      },
-    ];
+        // zowe files download uf
+        {
+          name: "FileDownload",
+          action: async function () {
+            const tmpDownloadPath = tmp.tmpNameSync();
+            const response = await Download.ussFile(
+              session,
+              `${testDefinition.unixDir}/test${userNumber}.txt`,
+              { file: tmpDownloadPath }
+            );
+            unlinkSync(tmpDownloadPath);
+            return response;
+          },
+        },
+        // zowe tso issue command
+        {
+          name: "TsoCommand",
+          action: async function () {
+            const response = await IssueTso.issueTsoCommand(
+              session,
+              testDefinition.accountCode,
+              `SEND 'Hello' USER(${userid})`
+            );
+            return response;
+          },
+        },
+        // zowe console issue command
+        {
+          name: "ConsoleCommand",
+          action: async function () {
+            return IssueCommand.issue(session, { command: "D IPLINFO" });
+          },
+        },
+        // zowe jobs submit
+        {
+          name: "JobSubmit",
+          action: async function () {
+            const job = await SubmitJobs.submitJcl(session, testJcl);
+            const cleanup = async function () {
+              await MonitorJobs.waitForOutputStatus(
+                session,
+                job.jobname,
+                job.jobid
+              );
+              await DeleteJobs.deleteJob(session, job.jobname, job.jobid);
+            };
+            const promise = cleanup();
+            return { success: true, job: job, cleanupPromise: promise };
+          },
+        },
+        // zowe jobs view
+        {
+          name: "JobView",
+          action: async function () {
+            const spoolFiles = await GetJobs.getSpoolFiles(
+              session,
+              testJobname,
+              testJobid
+            );
+            return { success: spoolFiles.length > 0, spoolFiles: spoolFiles };
+          },
+        },
+        // zowe jobs download
+        {
+          name: "JobDownload",
+          action: async function () {
+            const spoolContent = await GetJobs.getSpoolContentById(
+              session,
+              testJobname,
+              testJobid,
+              testSpoolId
+            );
+            return { success: spoolContent.length > 0, content: spoolContent };
+          },
+        },
+      ];
 
-    const tests: Test[] = [];
-    for (const test of allTests) {
-      if (testNames.indexOf(test.name) === -1) {
-        this.error(
-          `Internal error: ${test.name} is not a valid test name: ${testNames}`
-        );
-      }
-      if (
-        !testDefinition.selectedTestNames ||
-        testDefinition.selectedTestNames.indexOf(test.name) !== -1
-      ) {
-        tests.push(test);
-      }
-    }
-
-    let successfulRequests = 0;
-    let failedRequests = 0;
-    const startTime = new Date().getTime();
-    let requestNumber = 0;
-    let durations = [];
-    while (new Date().getTime() - startTime <= duration) {
-      const scriptStartTime = new Date().getTime();
-      for (const test of tests) {
-        requestNumber++;
-        loggerRequest.info(
-          `User #${userNumber} ${userid} - Request ${requestNumber} - Test ${test.name}: before action`
-        );
-        const commandStartTime = new Date().getTime();
-        let response;
-        try {
-          response = await test.action(); // eslint-disable-line no-await-in-loop
-        } catch (e) {
-          logger.warn(e);
-          response = {
-            success: false,
-            exception: e,
-          };
+      const tests: Test[] = [];
+      for (const test of allTests) {
+        if (testNames.indexOf(test.name) === -1) {
+          this.error(
+            `Internal error: ${test.name} is not a valid test name: ${testNames}`
+          );
         }
-        const commandEndTime = new Date().getTime();
-        const duration = commandEndTime - commandStartTime;
-        const responseString = JSON.stringify(response);
-        durations.push({
-          success: response.success,
-          duration: duration,
-          userNumber: userNumber,
-          testName: test.name,
-          requestNumber: requestNumber,
-        } as Duration);
-        if (response.success) {
-          successfulRequests++;
+        if (
+          !testDefinition.selectedTestNames ||
+          testDefinition.selectedTestNames.indexOf(test.name) !== -1
+        ) {
+          tests.push(test);
+        }
+      }
+
+      let successfulRequests = 0;
+      let failedRequests = 0;
+      const startTime = new Date().getTime();
+      let requestNumber = 0;
+      let durations = [];
+      while (new Date().getTime() - startTime <= duration) {
+        const scriptStartTime = new Date().getTime();
+        for (const test of tests) {
+          requestNumber++;
           loggerRequest.info(
-            `User #${userNumber} ${userid} - Request ${requestNumber} - Test ${test.name} successful in ${duration} ms: ${responseString}`
+            `User #${userNumber} ${userid} - Request ${requestNumber} - Test ${test.name}: before action`
           );
-        } else {
-          failedRequests++;
-          loggerRequest.error(
-            `User #${userNumber} ${userid} - Request ${requestNumber} - Test ${test.name} failed in ${duration} ms: ${responseString}`
-          );
+          const commandStartTime = new Date().getTime();
+          let response;
+          try {
+            response = await test.action(); // eslint-disable-line no-await-in-loop
+          } catch (e) {
+            logger.warn(e);
+            response = {
+              success: false,
+              exception: e,
+            };
+          }
+          const commandEndTime = new Date().getTime();
+          const duration = commandEndTime - commandStartTime;
+          const responseString = JSON.stringify(response);
+          durations.push({
+            success: response.success,
+            duration: duration,
+            userNumber: userNumber,
+            testName: test.name,
+            requestNumber: requestNumber,
+          } as Duration);
+          if (response.success) {
+            successfulRequests++;
+            loggerRequest.info(
+              `User #${userNumber} ${userid} - Request ${requestNumber} - Test ${test.name} successful in ${duration} ms: ${responseString}`
+            );
+          } else {
+            failedRequests++;
+            loggerRequest.error(
+              `User #${userNumber} ${userid} - Request ${requestNumber} - Test ${test.name} failed in ${duration} ms: ${responseString}`
+            );
+          }
+
+          await sleep(
+            Math.max(
+              commandDelay - (new Date().getTime() - commandStartTime),
+              0
+            )
+          ); // eslint-disable-line no-await-in-loop
         }
 
         await sleep(
-          Math.max(commandDelay - (new Date().getTime() - commandStartTime), 0)
+          Math.max(scriptDelay - (new Date().getTime() - scriptStartTime), 0)
         ); // eslint-disable-line no-await-in-loop
       }
 
-      await sleep(
-        Math.max(scriptDelay - (new Date().getTime() - scriptStartTime), 0)
-      ); // eslint-disable-line no-await-in-loop
+      this.log(
+        `Finished tests for ${userNumber} - ${userid} - successful: ${successfulRequests}, failed: ${failedRequests}`
+      );
+      await this.cleanupTestData(
+        session,
+        userNumber,
+        testJobname,
+        testJobid,
+        testDsn,
+        testUploadUssPath,
+        tmpCobolPath
+      );
+
+      return {
+        failedRequests: failedRequests,
+        successfulRequests: successfulRequests,
+        durations: durations,
+      };
+    } catch (e) {
+      logger.error(`Error processing tests for ${userNumber} - ${userid}`);
+      return {
+        failedRequests: 0,
+        successfulRequests: 0,
+        durations: [],
+      };
     }
-
-    this.log(
-      `Finished tests for ${userNumber} - ${userid} - successful: ${successfulRequests}, failed: ${failedRequests}`
-    );
-    await this.cleanupTestData(
-      session,
-      userNumber,
-      testJobname,
-      testJobid,
-      testDsn,
-      testUploadUssPath,
-      tmpCobolPath
-    );
-
-    return {
-      failedRequests: failedRequests,
-      successfulRequests: successfulRequests,
-      durations: durations,
-    };
   }
 
   private async cleanupTestData(
@@ -601,7 +622,8 @@ class Zztop extends Command {
         testDsn
       );
       if (!response.success) {
-        this.error(response.commandResponse, { exit: 2 });
+        logger.error(response.commandResponse);
+        throw Error(`Error preparing test dataset: ${testDsn}`);
       }
     }
     return testDsn;
